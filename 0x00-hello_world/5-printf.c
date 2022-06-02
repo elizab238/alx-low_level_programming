60aa0d4149a0b5fd5445ef6335e25af9b22cc8de
elizab238 <lizwanyoike20@gmail.com>
2022-06-02 12:37:32 -0700

    Using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Description: with proper grammar, but the outcome is a piece of art
+ * main - My Entry point
+ * Description: print with proper grammar, but the outcome is a piece of art,
  * Return: 0
-*/
+ */
 int main(void)
 {
-printf("\n,"with proper grammar, but the outcome is a piece of art,");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
