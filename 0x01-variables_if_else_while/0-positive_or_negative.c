c05936a5210954a102fd818f546e45e8cf486328
elizab238 <lizwanyoike20@gmail.com>
2022-06-04 10:15:05 -0700

    if else condition return zero, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - used to check the condition
+ * Description: Return zero, positive or negative
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -11,14 +16,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
        /* your code goes there */
 	if (n > 0)
-	{
-		        printf("%d is positive\n", n);
-	} else if (n == 0)
-	{
-		        printf("%d is zero\n", n);
-	} else if (n < 0)
-	{
-		        printf("%d is negative\n", n);
-	}
+{
+	printf("%d is positive\n", n);
+} else if (n == 0)
+{
+	printf("%d is zero\n", n);
+} else if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
 	return (0);
 }
