a4ec0ec05c0fa4490dcacdab7c42dae2841f3815
elizab238 <lizwanyoike20@gmail.com>
2022-06-05 04:47:24 -0700

    Pring single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - single digits of base 10
+ * Description - code must be in main function
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
