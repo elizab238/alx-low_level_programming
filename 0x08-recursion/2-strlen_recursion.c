15ef39b78d9d043413a6c0eb4a9ce2055f87b71f
elizab238 <lizwanyoike20@gmail.com>
2022-06-22 12:11:25 -0700

    printing the length of s tring in recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _strlen_recursion - size
+ *  @s: pointer to string params
+ *  Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+	{
+		return + 1 _strlen_recursion(s + 1);
+	}
+}
