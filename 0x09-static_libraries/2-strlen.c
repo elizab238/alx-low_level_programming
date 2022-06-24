6bcb718a5720371d17e96e61c59de837fd9de8fb
elizab238 <lizwanyoike20@gmail.com>
2022-06-24 12:47:24 -0700

    Static library

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -7,11 +7,10 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
 
-	while (s[ken] != '\0')
-	{
-		len++;
-	}
-	return (len);
+	return (inc);
 }
