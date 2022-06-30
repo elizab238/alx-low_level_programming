632ea7616e9c196cd29d0e8488771154938d3819
elizab238 <lizwanyoike20@gmail.com>
2022-06-30 11:43:23 -0700

    Rellocate a memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	else
 	{
-		 return (malloc(new_size));
+		return (malloc(new_size));
 	}
 	if (new_size == old_size)
 		return (ptr);
