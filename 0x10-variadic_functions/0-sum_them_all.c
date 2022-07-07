fe0c8b64d8cb17e681a89e762b0fbf977c646ae0
elizab238 <lizwanyoike20@gmail.com>
2022-07-07 12:21:19 -0700

    Return sum using variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list ptr;
+	va_list valist;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	var_start(ptr, n);
+	var_start(valist, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ptr, int);
-	va_end(ptr);
+		sum += va_arg(valist, int);
+	va_end(valist);
+
 	return (sum);
 }
