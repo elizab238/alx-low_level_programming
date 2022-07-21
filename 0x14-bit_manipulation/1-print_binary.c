3b6a1614610520d6ecde252f52f4a7855e2a8a8a
elizab238 <lizwanyoike20@gmail.com>
2022-07-21 13:11:17 -0700

    Binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,22 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int _putchar(char c);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
-
-#endif
+/**
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
